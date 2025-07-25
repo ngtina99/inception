9259033718d5cf46fde21185f1bbb60eb3bf51c0
ngtina99 <nguyen.valentina.99@gmail.com>
2025-07-25 02:45:10 +0200

    added incept files

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -2,12 +2,18 @@
 set -e
 
 # Read secrets from Docker secrets files if they exist
-#if [ -f /run/secrets/wp_admin_pass ]; then
-#  WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
-#fi
-#if [ -f /run/secrets/db_password ]; then
-#  WORDPRESS_DB_PASSWORD=$(cat /run/secrets/db_password)
-#fi
+if [ -f /run/secrets/wp_admin_pass ]; then
+  WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_pass)
+fi
+if [ -f /run/secrets/wp_user_pass ]; then
+  WP_USER_PASS=$(cat /run/secrets/wp_user_pass)
+fi
+if [ -f /run/secrets/wordpress_db_pass ]; then
+  WORDPRESS_DB_PASSWORD=$(cat /run/secrets/wordpress_db_pass)
+fi
+if [ -f /run/secrets/mysql_pass ]; then
+  MYSQL_PASSWORD=$(cat /run/secrets/mysql_pass)
+fi
 
 # Wait for MariaDB to be ready
 until mysql -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" -e ""; do
@@ -31,5 +37,18 @@ if ! wp core is-installed --allow-root; then
   wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASS" --role=author --allow-root
 fi
 
+# Ensure PHP-FPM listens on 0.0.0.0:9000 (TCP) instead of a Unix socket
+sed -i 's|listen = .*|listen = 0.0.0.0:9000|' /etc/php/7.4/fpm/pool.d/www.conf
+
 # Start php-fpm
+exec php-fpm7.4 -F
+
+echo "Before sed:"
+grep listen /etc/php/7.4/fpm/pool.d/www.conf
+
+sed -i 's|^listen = .*|listen = 0.0.0.0:9000|' /etc/php/7.4/fpm/pool.d/www.conf
+
+echo "After sed:"
+grep listen /etc/php/7.4/fpm/pool.d/www.conf
+
 exec php-fpm7.4 -F
